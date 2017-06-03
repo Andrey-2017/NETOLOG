4032fdc8a2e01c223a20f2d430dad8ec35946740
Andrey <palankin@mail.ru>
2017-06-03 22:37:31 +0300

    Внесены изменения по рекомендации преподавателя

--- HW_2_4/home_work_2-4.py
+++ HW_2_4/home_work_2-4.py
@@ -11,18 +11,19 @@ def get_first_list():
 def file_searching():
     files = get_first_list()
     while True:
-        print('Введите фрагмент названия файла для поиска (для выхода из программы введите - ё):')
-        name_fragm = input()
-        if name_fragm == 'ё':
+        print('Введите фрагмент строки из файла для поиска (для выхода из программы введите - ё):')
+        string_fragm = input()
+        if string_fragm == 'ё':
             break
-        else:
-            file_listing = list()
-            for file in files:
-                if name_fragm in file:
-                    print(file)
-                    file_listing.append(file)
-            files = file_listing
-            print('Всего файлов:', len(files))
+        file_listing = list()
+        for file_name in files:
+            with open(file_name) as f:
+                search_string = f.read()
+            if string_fragm in search_string:
+                print(file_name)
+                file_listing.append(file_name)
+        files = file_listing
+        print('Всего файлов: {0:d}'.format(len(files)))
 
 
 file_searching()
