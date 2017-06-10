6bd7b6637864dbfa7d673c6979b002e2274a6d08
Andrey <palankin@mail.ru>
2017-06-10 19:33:07 +0300

    По указанию преподавателя внесены изменения

--- HW_2_3/home_work_2-3.py
+++ HW_2_3/home_work_2-3.py
@@ -1,95 +1,60 @@
 import json
 
 
-def french_reading():
-    with open('newsfr.json', encoding='iso8859-5') as f:
+def files_reading(file_name, cods):
+    with open(file_name, encoding=cods) as f:
         d = json.load(f)
-    print('В новостях из Франции (хотя всё-таки из Кипра):')
-    return d
-
-
-def africa_reading():
-    with open('newsafr.json', encoding='utf-8') as f:
-        d = json.load(f)
-    print('В новостях из Африки:')
-    return d
-
-
-def cyprus_reading():
-    with open('newscy.json', encoding='koi8-r') as f:
-        d = json.load(f)
-    print('В новостях из Кипра:')
-    return d
-
-
-def italy_reading():
-    with open('newsit.json') as f:
-        d = json.load(f)
-    print('В новостях из Италии:')
     return d
 
 
 def countries_reading(country):
     if country == 'a':
-        d = africa_reading()
+        d = files_reading('newsafr.json', 'utf-8')
+        print('В новостях из Африки:')
         return d
     if country == 'c':
-        d = cyprus_reading()
+        d = files_reading('newscy.json', 'koi8-r')
+        print('В новостях из Кипра:')
         return d
     if country == 'f':
-        d = french_reading()
+        d = files_reading('newsfr.json', 'iso8859-5')
+        print('В новостях из Франции (хотя всё-таки из Кипра):')
         return d
     if country == 'i':
-        d = italy_reading()
+        d = files_reading('newsit.json', 'cp1251')
+        print('В новостях из Италии:')
         return d
 
 
 def get_pure_words():
-    country = input()
     feed = countries_reading(country)
-    if country == 'i':
-        words = ''
-        for descr in feed['rss']['channel']['item']:
+    words = ''
+    for descr in feed['rss']['channel']['item']:
+        if type(descr['description']) == str:
             words += descr['description']
-        words = words.split(' ')
-        pure_words = list()
-        for strings in words:
-            pure_word = strings.strip(',.()!/:;')
-            if len(pure_word) >= 6:
-                if 'href' not in pure_word:
-                    if '<br>' not in pure_word:
-                        if 'www' not in pure_word:
-                            pure_words.append(pure_word.lower())
-        return pure_words
-    else:
-        words = ''
-        for descr in feed['rss']['channel']['item']:
+        else:
             words += descr['description']['__cdata']
-        words = words.split(' ')
-        pure_words = list()
-        for strings in words:
-            pure_word = strings.strip(',.()!/:;')
-            if len(pure_word) >= 6:
-                if 'href' not in pure_word:
-                    if '<br>' not in pure_word:
-                        if 'www' not in pure_word:
-                            pure_words.append(pure_word.lower())
-        return pure_words
+    words = words.split(' ')
+    pure_words = list()
+    for strings in words:
+        pure_word = strings.strip(',.()!/:;')
+        if len(pure_word) >= 6:
+            if 'href' not in pure_word:
+                if '<br>' not in pure_word:
+                    if 'www' not in pure_word:
+                        pure_words.append(pure_word.lower())
+    return pure_words
 
 
 def repeat_massiv():
     pure_words = get_pure_words()
-    d = dict()
-    l = list()
+    word_frequencies = dict()
     for word in pure_words:
-        i = 0
-        if word not in l:
-            for strings in pure_words:
-                if strings == word:
-                    i += 1
-            d[word] = i
-            l = d.keys()
-    return d
+        if word in word_frequencies:
+            word_frequencies[word] += 1
+        else:
+            word_frequencies[word] = 1
+    return word_frequencies
 
 
 def get_top_ten():
@@ -104,43 +69,20 @@ def get_top_ten():
         for key in d.keys():
             if d[key] == value:
                 key_list.append(key)
-        key_list = key_list[:10]
+    key_list = key_list[:10]
     for index in range(len(key_list)):
-        print('слово-', key_list[index], '-встречается', d[key_list[index]], 'раз')
+        print('слово -{0:s}- встречается {1:d} раз'.format(key_list[index], d[key_list[index]]))
 
 
 def result():
+    global country
     print('Для выяснения Топ-10 слов в новостях:')
     print('из Африки введите - a')
     print('из Кипра введите - c')
     print('из Франции введите - f')
     print('из Италии введите - i')
+    country = input()
     get_top_ten()
 
 
 result()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
